f586eb36d4a79bacf1b071fc826a3d2d72f84aa9
Adam <adam@rajraj.sae>
2017-06-07 18:53:41 +0200

    initial

--- test/test.py
+++ test/test.py
@@ -1,1 +1,55 @@
+import discord
+from discord.ext import commands
+from .mcoc import class_color_codes
+from .utils.dataIO import dataIO
+from .utils.dataIO import fileIO
+from .utils import checks
+from operator import itemgetter, attrgetter
+from random import randint
+import time
+import os
+import ast
+import csv
+import requests
+import re
 
+class Test:
+
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.data_dir = 'data/test/users/{}/'
+        self.champs_file = self.data_dir + 'champs.json'
+        self.champ_re = re.compile(r'champ.*\.csv')
+
+
+    async def _on_attachment(self, msg):
+        channel = msg.channel
+        prefixes = tuple(self.bot.settings.get_prefixes(msg.server))
+        if not msg.attachments or msg.author.bot or msg.content.startswith(prefixes):
+            return
+        for attachment in msg.attachments:
+            if self.champ_re.match(attachment['filename']):
+                await self.bot.send_message(channel,
+                        "Found a CSV file to import.  Load new champions?  Type 'yes'.")
+                reply = await self.bot.wait_for_message(30, channel=channel,
+                        author=msg.author, content='yes')
+                if reply:
+                    await self._parse_champions_csv(msg, attachment)
+                else:
+                    await self.bot.send_message(channel, "Did not import")
+
+
+
+#-------------- setup -------------
+def check_folders():
+    if not os.path.exists("data/test/users"):
+        print("Creating data/test/users folder...")
+        os.makedirs("data/test/users")
+
+
+def setup(bot):
+    check_folders()
+    n = Test(bot)
+    bot.add_cog(n)
+    bot.add_listener(n._on_attachment, name='on_message')
