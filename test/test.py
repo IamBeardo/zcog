c5eb0233fd59ac8d7c95c993aa9eed070f07e33d
IamBeardo <adam@rajraj.se>
2018-06-15 12:00:53 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,14 +22,13 @@ class Test:
 
 
     @commands.command(pass_context=True)
-    async def mycom(self, ctx, *, tst)
-                    #user : discord.User = None):
+    async def mycom(self, ctx, user : discord.User = None):
         """This does stuff!"""
         if user is None:
             user=ctx.message.author
         
         #Your code will go here
-        await self.bot.say(" User: {} args: {}".format(user.name, tst))
+        await self.bot.say(" User: {} args: {}".format(user.name, "APA"))
 
 
     #@roster.command(pass_context=True, name='template')
