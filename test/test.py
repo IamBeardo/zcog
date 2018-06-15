d284161a39b243cc5987fd68bdad413a227d3481
IamBeardo <adam@rajraj.se>
2018-06-15 14:13:18 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ class Test:
     @commands.command(pass_context=True)
     async def renamewar(self, ctx, warName):
         pass
-        old = self.warinfo["Name"]
+        old = "olddd" #self.warinfo["Name"]
         await self.bot.say(" War [{}] renamed to [{}]".format(old, warName))
         
         
