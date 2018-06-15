da35ce7895ae57db9fa628810cf8911758902853
IamBeardo <adam@rajraj.se>
2018-06-15 10:53:38 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ class Test:
 
 
     @commands.command(pass_context=True)
-    async def mycom(self, ctx,  *, user : discord.User = None):
+    async def mycom(self, ctx,  *args, user : discord.User = None):
         """This does stuff!"""
         if user is None:
             user=ctx.message.author
